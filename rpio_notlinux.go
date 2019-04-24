1f579b32858a9b4848a7ffdb75a8e8abd7dcbfbb
Michael Mendelson <Michael.Mendelson@ipsoft.com>
2019-04-24 14:34:33 -0400

    added more stubs

--- rpio_notlinux.go
+++ rpio_notlinux.go
@@ -6,6 +6,28 @@ import "errors"
 
 type Pin uint8
 
+type State uint8
+
+const (
+	Low State = iota
+	High
+)
+
+
+type Mode uint8
+
+const (
+	Input Mode = iota
+	Output
+	Clock
+	Pwm
+	Spi
+)
+
+func (p Pin) Mode(m Mode) {
+
+}
+
 func (p Pin) Low() {
 
 }
@@ -16,6 +38,14 @@ func (p Pin) High() {
 
 func (p Pin) Output() {
 
+}
+
+func (p Pin) Input() {
+
+}
+
+func (p Pin) Read() State {
+	return Low
 }
 
 func Open() (error) {
